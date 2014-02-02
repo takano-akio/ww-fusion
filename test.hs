cbc9520e16726d3f4e1749f01699c0ee5c0b0a7e
Takano Akio <aljee@hyper.cx>
2014-02-03 07:14:48 +0900

    test.hs: add serializeTree example

--- test.hs
+++ test.hs
@@ -1,8 +1,15 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UnboxedTuples #-}
+{-# LANGUAGE MagicHash #-}
 module F where
 --import qualified Data.IntMap as IM
 import WWFusion as WW
+import GHC.Exts(Ptr(..), Addr#, RealWorld, State#)
+import GHC.IO(IO(..), unIO)
+import Foreign.Ptr
+import Foreign.Storable
+import Foreign.Marshal.Array
 
 f :: [[Int]] -> Int
 f x = foldl' (+) 0 $ WW.concat x
@@ -12,6 +19,16 @@ g x y = foldl' (+) 0 $ x WW.++ y
 
 h :: Tree -> Int
 h t = WW.foldl (+) 0 $ toList t
+
+-- | Serialize a tree into a buffer. The buffer is assumed to
+-- be large enough.
+serializeTree :: Tree -> Ptr () -> IO (Ptr ())
+serializeTree t buf = foldIO_Ptr write buf $ toList t
+  where
+    write p x = poke p' x >> return (castPtr $ advancePtr p' 1)
+      where
+        p' :: Ptr Int
+        p' = castPtr p
 
 data Tree
   = Bin !Tree !Tree
@@ -33,3 +50,19 @@ toListFB root (Wrap wrap unwrap) c n = wrap go root n
       Bin l r -> wrap go l (wrap go r z)
       Tip x -> c x z
 {-# INLINE [0] toListFB #-}
+
+newtype IOInt e = IOInt (e -> Addr# -> State# RealWorld -> (# State# RealWorld, Addr# #))
+
+-- | A version of foldM, specialized for the IO monad and the
+-- accumulator type of Ptr (). It uses unboxed ints to pass around
+-- intermediate results.
+foldIO_Ptr :: (Ptr () -> a -> IO (Ptr ())) -> Ptr () -> [a] -> IO (Ptr ())
+foldIO_Ptr f initial = \xs -> foldrW (Wrap wrap unwrap) g return xs initial
+  where
+    wrap (IOInt h) e k (Ptr a) = io >>= k
+      where
+        io = IO $ \s -> case h e a s of
+          (# s', a' #) -> (# s', Ptr a' #)
+    unwrap u = IOInt $ \e a s -> case unIO (u e return (Ptr a)) s of
+      (# s', Ptr a' #) -> (# s', a' #)
+    g x next acc = next =<< f acc x
